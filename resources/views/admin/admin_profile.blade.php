0711903207b0934944bc7b5078df8e2ecda277ba
elyasdehati <elyas786dehaty@gmail.com>
2025-07-10 19:24:18 +0430

    Updatee Profile is done

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -93,7 +93,7 @@
         <div class="form-group mb-3 row">
             <label class="form-label"></label>
             <div class="col-lg-12 col-xl-12">
-                <img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" class="rounded-circle avatar-xxl img-thumbnail float-start" alt="image profile">
+                <img id="showImage" src="{{ ( !empty($profileData->photo )) ? url('upload/user_images/'.$profileData->photo) : url('upload/no_image.jpg') }}" class="rounded-circle avatar-xxl img-thumbnail float-start" alt="image profile">
             </div>
         </div>
 
