4728c2cf52edfb4f6c88d058ee188ec3345c8c69
elyasdehati <elyas786dehaty@gmail.com>
2025-07-16 09:15:07 +0430

    Team Page Update, Delete is done

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\HomeController;
 use App\Http\Controllers\Backend\ReviewController;
 use App\Http\Controllers\backend\SliderController;
+use App\Http\Controllers\Backend\TeamController;
 use App\Http\Controllers\FrontendController;
 
 Route::get('/', function () {
@@ -99,6 +100,15 @@ Route::middleware('auth')->group(function () {
     Route::controller(HomeController::class)->group(function () {
         Route::post('/update-app/{id}', 'UpdateApps');
         Route::post('/update-app-image/{id}', 'UpdateAppsImage');
+    });
+
+    Route::controller(TeamController::class)->group(function () {
+        Route::get('/all/team','AllTeam')->name('all.team');
+        Route::get('/add/team','AddTeam')->name('add.team');
+        Route::post('/store/team','StoreTeam')->name('store.team');
+        Route::get('/edit/team/{id}', 'EditTeam')->name('edit.team');
+        Route::post('/update/team', 'UpdateTeam')->name('update.team');
+        Route::get('/delete/team/{id}', 'DeleteTeam')->name('delete.team');
     });
 });
 
